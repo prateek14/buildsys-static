c9052f872191c02a21a867475ead0d3593c24dc6
Prateek Arora <prateek.arora@live.com>
2020-11-26 13:18:29 +0530

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const appConfig = {
     entry: './src/index.tsx',
     output: {
-        path: __dirname + '/public',
+        path: __dirname + '/public/build',
         filename: 'index.js',
     },
     resolve: {
@@ -52,7 +52,7 @@ const appConfig = {
 const serviceWorkerConfig = {
     entry: './src/service-worker.ts',
     output: {
-        path: __dirname + '/public',
+        path: __dirname + '/public/build',
         filename: 'service-worker.js',
     },
     resolve: {
