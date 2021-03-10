bb4f241585e41e2d9030391acbc227e93e66ede7
Prateek Arora <prateek.arora@live.com>
2021-03-10 10:31:07 +0530

    blog css

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -18,7 +18,7 @@ declare const self: ServiceWorkerGlobalScope;
 
 clientsClaim();
 
-const version = '38';
+const version = '39';
 
 self.__WB_MANIFEST = [
     { url: '/index.html', revision: version },
