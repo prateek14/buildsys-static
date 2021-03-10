0420ec3600fb41c675ba4346ffcf2401cd41fed0
Prateek Arora <prateek1.55@gmail.com>
2021-03-10 10:05:35 +0530

    Update service-worker.ts

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -18,7 +18,7 @@ declare const self: ServiceWorkerGlobalScope;
 
 clientsClaim();
 
-const version = '36';
+const version = '37';
 
 self.__WB_MANIFEST = [
     { url: '/index.html', revision: version },
