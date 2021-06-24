9c206d529ab304f9a3190f582f8ddfecaa502040
amishaepistle <86294192+amishaepistle@users.noreply.github.com>
2021-06-24 12:09:44 +0530

    Update service-worker.ts

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -18,7 +18,7 @@ declare const self: ServiceWorkerGlobalScope;
 
 clientsClaim();
 
-const version = '46';
+const version = '47';
 self.__WB_MANIFEST = [
     { url: '/index.html', revision: version },
     { url: '/index.js', revision: version },
