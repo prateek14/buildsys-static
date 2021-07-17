c0629c7ab4f1a1e9b74fefb30373db134bf3fbff
hey24sheep <hey24sheep@gmail.com>
2021-07-17 15:43:15 +0530

    sw version update

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -18,7 +18,7 @@ declare const self: ServiceWorkerGlobalScope;
 
 clientsClaim();
 
-const version = '47';
+const version = '48';
 self.__WB_MANIFEST = [
     { url: '/index.html', revision: version },
     { url: '/index.js', revision: version },
